1905330e34fd7771789fce4b3d818499b16bc3cc
abirbhadra <58373108+abirbhadra@users.noreply.github.com>
2023-08-22 03:09:40 +0530

    Animation controller with keys
    Space key for jump, left Ctrl for crouch, left shift for Run

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private bool run ;
+    private bool crouch;
+    private bool jump;
+
     public Animator animator;
     void Update()
     {
@@ -20,5 +24,23 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale; // here transforming the scale with new scale value we got
+
+        animator.SetBool("Run", run );//Run animation
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            run = true;
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            run = false;
+
+        animator.SetBool("Crouch", crouch);//Crouch Animation
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+            crouch = true;
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+            crouch = false;
+
+        animator.SetBool("Jump", jump);//Crouch Animation
+        if (Input.GetKeyDown(KeyCode.Space))
+            jump = true;
+        if (Input.GetKeyUp(KeyCode.Space))
+            jump = false;
     }
 }
