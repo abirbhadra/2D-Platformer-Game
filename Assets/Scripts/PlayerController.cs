87219275613fe3a7231451b0fabb7cba101ae5d1
abirbhadra <58373108+abirbhadra@users.noreply.github.com>
2023-08-24 02:35:53 +0530

    Horizontal Movement Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,28 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private bool run ;
-    private bool crouch;
-    private bool jump;
 
-    public Animator animator;
-    void Update()
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private bool run;
+    [SerializeField] private bool crouch;
+    [SerializeField] private bool jump;
+    [SerializeField] private float speed;
+    public void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed",Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        PlayMovementAnimations(horizontal);
+        MoveCharacter(horizontal);
+
+    }
+
+    private void MoveCharacter(float horizontal) // character movement
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void PlayMovementAnimations(float horizontal)
+    {
+        animator.SetFloat("Speed",Mathf.Abs(horizontal));
 
         Vector3 scale = transform.localScale;
-        if (speed<0)
+        if (horizontal<0)
         {
-            scale.x = -1f * Mathf.Abs(scale.x);
+        scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if(speed>0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale; // here transforming the scale with new scale value we got
 
-        animator.SetBool("Run", run );//Run animation
+        animator.SetBool("Run", run);//Run animation
         if (Input.GetKeyDown(KeyCode.LeftShift))
             run = true;
         if (Input.GetKeyUp(KeyCode.LeftShift))
@@ -42,5 +58,7 @@ public class PlayerController : MonoBehaviour
             jump = true;
         if (Input.GetKeyUp(KeyCode.Space))
             jump = false;
+                   
     }
+
 }
