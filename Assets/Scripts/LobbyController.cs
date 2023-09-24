f79bd466766bf2ee061bfed3c9e3e56af1ae9177
abirbhadra <58373108+abirbhadra@users.noreply.github.com>
2023-09-25 01:41:04 +0530

    Lobby UI with Play button to start level 1

---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class LobbyController : MonoBehaviour
+{
+    public Button buttonPlay;
+    public string Scene;
+
+    public Button buttonRestart;
+    private void Awake()
+    {
+        buttonPlay.onClick.AddListener(ReloadLevel);
+    }
+
+    public void ReloadLevel()
+    {
+
+            SceneManager.LoadScene(1);
+        
+    }
+}
